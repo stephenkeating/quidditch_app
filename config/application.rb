3663c21c7be567194bbdc96e2dfbe4449fd1710e
github username <github email address>
2019-11-14 16:13:13 -0500

    added font and bootstrap

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,8 @@ require_relative 'boot'
 
 require 'rails/all'
 
+
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -10,6 +12,8 @@ module QuidditchApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.assets.enabled = true  
+    config.assets.paths << "/app/assets/fonts/webfonts"
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
